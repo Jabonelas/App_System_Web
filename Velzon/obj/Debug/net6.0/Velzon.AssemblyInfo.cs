e7702b7ec3af2f22afc9e34dc99dffd2c6894ebb
israel <israel094@hotmail.com>
2024-12-15 19:30:57 -0300

    tela de consumidor

--- Velzon/obj/Debug/net6.0/Velzon.AssemblyInfo.cs
+++ Velzon/obj/Debug/net6.0/Velzon.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Velzon")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe3dd0fbe4842ac1256b3b2fb0753c833ee6825f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4658e22ad86f973aefc51d346ccf03ebed43e9b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Velzon")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Velzon")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
